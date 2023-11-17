f70370582268c03d20d217486b9c65cc4e540b81
Louane Lesur <Louane.Lesur@etu.univ-grenoble-alpes.fr>
2023-11-17 12:33:54 +0100

    Objectif 1

---
+++ src/prodcons/v1/ProdConsBuffer.java
@@ -0,0 +1,78 @@
+package prodcons.v1;
+
+import BaseProdConso.IProdConsBuffer;
+import BaseProdConso.Message;
+
+/* Variables :
+ * 		-
+ * 		-
+ * 
+ * 
+ * 
+ * Tableau de garde-actions:
+ * 
+ *     Opérations    | Pré-action |  Garde   |    Post-action
+ * --------------------------------------------------------------
+ * Produce(Message m)|            | nempty>0 | nfull++; nempty--;
+ * --------------------------------------------------------------
+ * Message Consume   |            | nfull>0  | nfull--; nempty++;
+*/
+
+public class ProdConsBuffer implements IProdConsBuffer {
+	int nfull;
+	int nempty;
+	Message[] buffer;
+	int bufSz;
+	int in;
+	int out;
+
+	public ProdConsBuffer(int bufSz) {
+		this.nfull = 0;
+		this.nempty = bufSz;
+		buffer = new Message[bufSz];
+		this.bufSz = bufSz;
+		in = 0;
+		out = 0;
+	}
+
+	@Override
+	public synchronized void put(Message m) throws InterruptedException {
+		while (nempty <= 0) {
+			try {
+				wait();
+			} catch (InterruptedException e) {
+			}
+		}
+		buffer[in] = m;
+		in = (in+1)%bufSz; 
+		nfull++;
+		nempty--;
+	}
+
+	@Override
+	public synchronized Message get() throws InterruptedException {
+		while (nfull <= 0) {
+			try {
+				wait();
+			} catch (InterruptedException e) {
+			}
+		}
+		Message tmp = buffer[out];
+		buffer[out] = null;
+		out = (out+1)%bufSz;
+		nfull--;
+		nempty++;
+		return tmp;
+	}
+
+	@Override
+	public int nmsg() {
+		return nempty;
+	}
+
+	@Override
+	public int totmsg() {
+		return nfull;
+	}
+
+}
